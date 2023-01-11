ccbf5887440ab8887457c60702aec80cca2fa665
Rod7101 <118309284+Rod7101@users.noreply.github.com>
2023-01-10 20:39:49 -0600

    Create text_adventure.js

---
+++ U5_conditionals/5.6 Text Adventure/text_adventure.js
@@ -0,0 +1,16 @@
+alert("You have a Math test tomorow morning. Will you pass the test or fail?")
+
+let study_or_movies = prompt("do you (1) go study and go to sleep early, or do you (2) stay up late watching movies? Please type 1 or 2!")
+
+if(study_or_movies == 1){
+    alert("You PASSED your test!")
+} else {
+    alert("You decide to set an alarm early so you can study the next morning?")
+let study_or_dont = prompt("do you (1) wake up on time to study in the morning, or do you (2) hit snooze and sleep in and run late to class?")
+    if(study_or_dont==1){
+        alert("You PASSED by only a few points!")
+    } else {
+        alert("You FAILED your test!")
+    }
+    
+} 
